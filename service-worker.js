d79a24e71595d086a9ae10e38c984c0304aad8db
EnriqueCesar <ecesar.alsea01@gmail.com>
2026-07-14 17:21:42 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='compras-centro-norte-v1';
+const CACHE='compras-centro-norte-v2';
 const ASSETS=['./','./index.html','./css/styles.css','./js/app.js','./vendor/xlsx.full.min.js','./manifest.json','./assets/images/compras.png','./assets/icons/icon.svg','./assets/icons/icon-192.png','./assets/icons/icon-512.png','./data/Compras_Dtto_EC.xlsx'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
